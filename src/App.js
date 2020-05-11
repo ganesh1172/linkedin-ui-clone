c6c4c17bdeca864d938075c258a7ad289cbabf54
ganesh1172 <ganesh152595@gmail.com>
2020-05-11 20:27:09 +0530

    import footer component to display in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import './App.css';
+import Footer from './component/footer'
 function App() {
   return (
     <div id="app_container">
       <header className="head"></header>
-      <Container max-width="sm">
-        <h2>hello</h2>
-      </Container>
+      <div className="app_content">
+        <Container max-width="sm">
+          <h2>hello</h2>
+        </Container>
+      </div>
+      <Footer />
     </div>
   );
 }
