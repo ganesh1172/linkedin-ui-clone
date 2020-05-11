140f8f63472e97b14577175a74c9a095a1cd4a67
ganesh1172 <ganesh152595@gmail.com>
2020-05-11 19:39:08 +0530

    import container from material-ui/core

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import { Container } from '@material-ui/core';
+import './App.css';
 function App() {
   return (
-    <div className="App">
+    <div id="app_container">
+      <header className="head"></header>
+      <Container max-width="sm">
+        <h2>hello</h2>
+      </Container>
     </div>
   );
 }
-
 export default App;
