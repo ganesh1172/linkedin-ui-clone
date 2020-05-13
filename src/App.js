fc24e52dae88aa670d74e4b3cc48311f8ee1ade8
ganesh1172 <ganesh152595@gmail.com>
2020-05-13 11:32:26 +0530

    added form and styles for form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, form, TextField } from '@material-ui/core';
+import { Container, Typography, TextField, Button, Link } from '@material-ui/core';
 import './App.css';
 import 'typeface-roboto';
 import Footer from './component/footer';
@@ -12,6 +12,34 @@ const useStyles = makeStyles({
   },
   title2: {
     paddingBottom: 32,
+  },
+  text_style: {
+    width: '45ch',
+    marginBottom: 12,
+  },
+  btn_style: {
+    width: '360px',
+    padding: '12px 0',
+    marginTop: '8px',
+    textTransform: 'initial',
+    '&:hover': {
+      backgroundColor: '#006097',
+      color: '#fff',
+    }
+  },
+  btn_forpass: {
+    textTransform: 'initial',
+    '&:hover': {
+      background: "#E5F5FC",
+      color: '#006097',
+    },
+    marginTop: '24px',
+  },
+  new_join: {
+    marginTop: '12px',
+  },
+  j_Now: {
+    textDecoration: 'none',
   }
 });
 
@@ -30,8 +58,21 @@ function App() {
             Don't miss your next opportunity. Sign in to stay updated on your professional world.
           </Typography>
           <form>
-            <TextField fullWidth></TextField>
+            <TextField id="standard-basic" label="Email or Phone" medium variant="filled" className={classes.text_style} /><br />
+            <TextField id="filled-basic" label="Email or Phone" small variant="filled" className={classes.text_style} />
+            <br />
+            <Button variant="contained" size="large" color="primary" className={classes.btn_style}>
+              Sign in
+            </Button>
           </form>
+          <div>
+            <Button color="primary" className={classes.btn_forpass}>Forgot password?</Button>
+            <Typography variant="body1" className={classes.new_join}>
+              New to LinkedIn? <Link href="#" className={classes.j_now}>
+                Join now
+      </Link>
+            </Typography>
+          </div>
         </Container>
       </div>
       <Footer />
