c772313ee0201ab57c2cd717c765bd6bdc731b35
ganesh1172 <ganesh152595@gmail.com>
2020-05-14 09:45:13 +0530

    add image to footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Container, Typography } from '@material-ui/core';
 import '../App.css';
 import 'typeface-roboto';
+import logo from '../Image/footer_logo.png';
 
 function Footer() {
     return (
         <div className="footer">
             <Container max-width="xs" >
                 <div className="footer_wrapper">
-                    <img src="/image/Icon.png" alt="Logo" className="footer_logo" />
+                    <img src={logo} alt="Logo" className="footer_logo" />
                     <Typography variant="body2" className="copyright">
                         &copy; 2020
                     </Typography>
