5067a2a371b4f5800de752ecca020462370adfe7
ganesh1172 <ganesh152595@gmail.com>
2020-05-16 09:40:19 +0530

    added new file joinNow and set style

---
+++ src/component/joinNow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from './footer';
+import './joinNow.css';
+
+function JoinNow() {
+    return (
+        <div id="app_container">
+            <Footer />
+        </div>
+    );
+}
+export default JoinNow;
