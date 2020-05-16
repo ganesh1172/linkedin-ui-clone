0d969c01f89fcca2e7d16c83d147dd8c65911a23
ganesh1172 <ganesh152595@gmail.com>
2020-05-16 09:36:16 +0530

    added material-ui component, changes in btn_signin style and imported react router package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, TextField, Button, Link } from '@material-ui/core';
+import { Container, Typography, TextField, Button } from '@material-ui/core';
 import './App.css';
 import 'typeface-roboto';
 import Footer from './component/footer';
@@ -7,6 +7,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import useForm from "./useForm";
 import validate from './LoginFormValidationRules';
 import Img from './Image/header_logo.png';
+import ForgotPassword from './component/forgotPassword';
+import JoinNow from './component/joinNow';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,15 +20,16 @@ const useStyles = makeStyles((theme) => ({
   title2: {
     paddingBottom: 32,
   },
-  text_style: {
+  text_input: {
     width: '45ch',
     [theme.breakpoints.down('sm')]: {
       width: '100%',
     },
     marginBottom: 12,
+    color: '#fff',
   },
-  btn_style: {
-    width: '360px',
+  btn_signin: {
+    width: '400px',
     [theme.breakpoints.down('sm')]: {
       width: '100%',
     },
@@ -47,9 +51,6 @@ const useStyles = makeStyles((theme) => ({
   },
   new_join: {
     marginTop: '12px',
-  },
-  j_Now: {
-    textDecoration: 'none',
   }
 }));
 
@@ -67,43 +68,52 @@ function App() {
     console.log('No errors, submit callback called!');
   }
   return (
-    <div id="app_container">
-      <header className="head">
-      </header>
-      <div className="app_content">
-        <Container max-width="sm">
-          <img src={Img} alt="logo" className="header_logo" />
-          <Typography variant="h5" className={classes.title}>Welcome Back</Typography>
-          <Typography variant="body1" color="textSecondary" className={classes.title2}>
-            Don't miss your next opportunity. Sign in to stay updated on your professional world.
-          </Typography>
-          <form onSubmit={handleSubmit} noValidate>
-            <TextField id="standard-basic" label="Email or Phone" type="email" name="email" variant="filled" className={classes.text_style} onChange={handleChange} value={values.email || ''} required />
-            {errors.email && (
-              <p className="help">{errors.email}</p>
-            )}
-            <br />
-            <TextField id="filled-basic" label="Password" type="password" name="password" variant="filled" className={classes.text_style} onChange={handleChange} value={values.password || ''} required />
-            {errors.password && (
-              <p className="help">{errors.password}</p>
-            )}
-            <br />
-            <Button variant="contained" size="large" color="primary" type="submit" className={classes.btn_style}>
-              Sign in
-            </Button>
-          </form>
-          <div>
-            <Button color="primary" className={classes.btn_forpass}>Forgot password?</Button>
-            <Typography variant="body1" className={classes.new_join}>
-              New to LinkedIn? <Link href="/forgetPassword" className={classes.j_now}>
-                Join now
-      </Link>
+    <Router>
+      <div id="app_container">
+        <header className="head"></header>
+        <div className="app_content">
+          <Container max-width="sm">
+            <img src={Img} alt="logo" className="header_logo" />
+            <Typography variant="h5" className={classes.title}>Welcome Back</Typography>
+            <Typography variant="body1" color="textSecondary" className={classes.title2}>
+              Don't miss your next opportunity. Sign in to stay updated on your professional world.
             </Typography>
-          </div>
-        </Container>
+            <form onSubmit={handleSubmit} noValidate>
+              <TextField variant="outlined" label="Email or Phone" type="email" name="email" className={classes.text_input} onChange={handleChange} value={values.email || ''} required />
+              {errors.email && (
+                <p className="help">{errors.email}</p>
+              )}
+              <br />
+              <TextField variant="outlined" label="Password" type="password" name="password" className={classes.text_input} onChange={handleChange} value={values.password || ''} required />
+              {errors.password && (
+                <p className="help">{errors.password}</p>
+              )}
+              <br />
+              <Button variant="contained" size="large" color="primary" type="submit" className={classes.btn_signin}>
+                Sign in
+              </Button>
+            </form>
+            <div>
+              <Button color="primary" className={classes.btn_forpass}>
+                <Link to="/ForgotPassword" target="_self" style={{ textDecoration: "none" }}>Forgot password?</Link></Button>
+              <Typography variant="body1" className={classes.new_join}>
+                New to LinkedIn? <Link to="/JoinNow" style={{ textDecoration: "none" }}>
+                  Join now </Link>
+              </Typography>
+            </div>
+          </Container>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+      <Switch>
+        <Route path="/ForgotPassword">
+          <ForgotPassword />
+        </Route>
+        <Route path="/JoinNow">
+          <JoinNow />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 export default App;
