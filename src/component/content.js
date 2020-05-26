6e81af295b2af01d708d190f45c2706e75ce75e9
ganesh1172 <ganesh172595@gmail.com>
2020-05-26 17:53:59 +0530

    export testimonials data to seperate file

---
+++ src/component/content.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        header: 'Noel got a job after being unemployed',
+        description: '“After I lost my job, one of the first things I did was update my LinkedIn profile. I had a recruiter reach out to me and I ended up getting the job."',
+        image: require("../Image/noel_debow.jpg"),
+        user: 'Noel Debow',
+        userProfile: 'Visual Merchandiser',
+    },
+    {
+        header: 'Aaron was matched with a recruiter',
+        description: '“I was having a very hard time finding a job. A supervisor reached out to me on LinkedIn. We set up an interview and from there I was hired. LinkedIn was the matchmaker.”',
+        image: require('../Image/aaron_pagan.jpg'),
+        user: 'Aaron Pagan',
+        userProfile: 'Accessibility Technology Sales',
+    },
+    {
+        header: 'Curtis regained his confidence',
+        description: '“I didn’t think I could make it in the city. I went on LinkedIn and found a lot of jobs  - I realized, I am qualified. So, I applied. And I got it!”',
+        image: require('../Image/curtis_pagan.jpg'),
+        user: 'Curtis Pagan',
+        userProfile: 'Non-profit Marketer',
+    },
+]
