961fc7225a9de4116479c9758feccc12d1de8e8b
ganesh1172 <ganesh172595@gmail.com>
2020-06-04 22:16:19 +0530

    Updates

---
+++ precache-manifest.43ecd5c5be9b85d7cd7d098e61273790.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065e01232443218ae7675d601bb59813",
+    "url": "/linkedin-ui-clone/index.html"
+  },
+  {
+    "revision": "31ff72c80fc6698dc794",
+    "url": "/linkedin-ui-clone/static/css/2.b0d70484.chunk.css"
+  },
+  {
+    "revision": "997de5c0f3bdcc314639",
+    "url": "/linkedin-ui-clone/static/css/main.6955a97e.chunk.css"
+  },
+  {
+    "revision": "31ff72c80fc6698dc794",
+    "url": "/linkedin-ui-clone/static/js/2.5aff94d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/linkedin-ui-clone/static/js/2.5aff94d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997de5c0f3bdcc314639",
+    "url": "/linkedin-ui-clone/static/js/main.14a7c7ed.chunk.js"
+  },
+  {
+    "revision": "8633cf210d87ad93ec6a",
+    "url": "/linkedin-ui-clone/static/js/runtime-main.e748f43c.js"
+  },
+  {
+    "revision": "09b831bb1689bbb01a09dba24a866103",
+    "url": "/linkedin-ui-clone/static/media/aaron_pagan.09b831bb.jpg"
+  },
+  {
+    "revision": "f435a83ed82f60afe755b634353b971a",
+    "url": "/linkedin-ui-clone/static/media/brand_discovery_image.f435a83e.jpg"
+  },
+  {
+    "revision": "40928c2e1cb5652bfaecb75ce156851d",
+    "url": "/linkedin-ui-clone/static/media/curtis_pagan.40928c2e.jpg"
+  },
+  {
+    "revision": "d727c5e51e8f1fc7f79affc8c0d06920",
+    "url": "/linkedin-ui-clone/static/media/directory_logo.d727c5e5.svg"
+  },
+  {
+    "revision": "5e75383ddec05524df13a5b53b748f24",
+    "url": "/linkedin-ui-clone/static/media/down_arrow.5e75383d.svg"
+  },
+  {
+    "revision": "223cbfd62f27022d59d7be7a8d2b3302",
+    "url": "/linkedin-ui-clone/static/media/footer_logo.223cbfd6.png"
+  },
+  {
+    "revision": "30b56ef502b0f472638681ea94bfbffd",
+    "url": "/linkedin-ui-clone/static/media/get_started.30b56ef5.svg"
+  },
+  {
+    "revision": "64fb49de3f46cefee8fb089d786468ea",
+    "url": "/linkedin-ui-clone/static/media/home_desktop_size.64fb49de.svg"
+  },
+  {
+    "revision": "c1cb0073467d33681995c75146251ad9",
+    "url": "/linkedin-ui-clone/static/media/home_logo.c1cb0073.svg"
+  },
+  {
+    "revision": "37ea66802cb3b1fce1f94d8305f26630",
+    "url": "/linkedin-ui-clone/static/media/home_mobile_size.37ea6680.svg"
+  },
+  {
+    "revision": "9139dbc53375526441099b13546853b7",
+    "url": "/linkedin-ui-clone/static/media/join_logo.9139dbc5.png"
+  },
+  {
+    "revision": "e52d14519d0790ec76821dc3db69183b",
+    "url": "/linkedin-ui-clone/static/media/learning.e52d1451.svg"
+  },
+  {
+    "revision": "ba7aeda7c8806a856959d7dc131b3693",
+    "url": "/linkedin-ui-clone/static/media/meeting.ba7aeda7.svg"
+  },
+  {
+    "revision": "afa002ea8c9c018f56ab4f55bb249b87",
+    "url": "/linkedin-ui-clone/static/media/more_arrow.afa002ea.svg"
+  },
+  {
+    "revision": "e4b935ede86c7a2fe6b13e338c10d7f5",
+    "url": "/linkedin-ui-clone/static/media/noel_debow.e4b935ed.jpg"
+  }
+]);
